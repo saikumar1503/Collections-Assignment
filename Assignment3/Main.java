7b59fa4b3a18e06a4a2123fc28d88efbf55e628a
saikumar1503 <83894888+saikumar1503@users.noreply.github.com>
2023-09-20 20:28:43 +0530

    Main.java

--- Assignment3/Main.java
+++ Assignment3/Main.java
@@ -1,140 +1,127 @@
+
+
 package Assignment2;
 
+import java.util.*;
 
 
-import java.util.HashMap;
 
-import java.util.Iterator;
-
-import java.util.List;
-
-import java.util.Map;
 	public class Main {
+		
+		public static void main(String[] args) {
 
-			
-
-			
+			 Account a1 = new Account(123,"jawahar",2000,"bangalore");
 
 	 
 
-			public static void main(String[] args) {
+			Account a2 = new Account(234,"varun",3000,"chennai");
 
-				Map<Integer, Account> accountMap = new HashMap<>();
+	 
 
-				
+			Account a3 = new Account(456,"dinesh",5000,"mumbai");
 
-				Account a1 = new Account(1,"saikumar",121,2000,"bangalore");
+	 
 
-				Account a2 = new Account(2,"vinda",222,3000,"chennai");
+			Account a4 = new Account(567,"thinesh",4500,"coimbatore");
 
-				Account a3 = new Account(3,"dinesh",234,5000,"mumbai");
+	 
 
-				Account a4 = new Account(4,"latha",4500,500,"bangalore");
+			Account a5 = new Account(345,"dharani",7688,"bangalore");
 
-				Account a5 = new Account(5,"hjhfj",7688,456,"bangalore");
+	 
 
-				Account a6 = new Account(6,"fdsf",9876,230,"bangalore");
+			Account a6 = new Account(888,"harish",9876,"bangalore");
 
-						
+	 
 
-				
+			  Map<Integer,Account> accountMap = new HashMap<>();
 
-				accountMap.put(a1.getAccountId(), a1);
+	 
 
-				accountMap.put(a2.getAccountId(), a2);
+			  accountMap.put(a1.getAccountNumber(), a1);
 
-				accountMap.put(a3.getAccountId(), a3);
+	 
 
-				accountMap.put(a4.getAccountId(), a4);
+			  accountMap.put(a2.getAccountNumber(), a2);
 
-				accountMap.put(a5.getAccountId(), a5);
+	 
 
-				accountMap.put(a6.getAccountId(), a6);
+			  accountMap.put(a3.getAccountNumber(), a3);
 
-				
+	 
 
-				System.out.println("key"+"   "+"Value");
+			  accountMap.put(a4.getAccountNumber(), a4);
+			  accountMap.put(a5.getAccountNumber(), a5);
 
-		        for(Map.Entry<Integer, Account> entry : accountMap.entrySet()) {
+	 
 
-		        	Integer Key = entry.getKey();
+			  Scanner sc = new Scanner(System.in);
 
-		        	Account val = entry.getValue();
+	 
 
-		        	System.out.println(Key+" "+val);
+			  System.out.println("Enter key to get account details");
 
-		        }
+	 
 
-				System.out.println("-----ByLocation-----------");
+			  int key = sc.nextInt();
 
-				System.out.println("Key"+"  "+"Value");
+	 
 
-				System.out.println("");
+			 Account acc = accountMap.get(key);
 
-				for(Map.Entry<Integer, Account> entry : accountMap.entrySet()) {
+	 
 
-					Integer Key = entry.getKey();
+			  System.out.println(acc.toString());
 
-		        	Account val = entry.getValue();
+	 
 
-		        	if(val.getLocation() == "Bangalore") {
+			  System.out.println("Enter location to get account details");
 
-		        		System.out.println(" ");
+	 
 
-		        		System.out.println(Key+"  "+val.getLocation());
+			  String location = sc.next();
 
-		        		
+	 
 
-		        	}
+			  Map<String,List<Account>> newAccount = new HashMap<>();
 
-		        		
+	 
 
-		        	}
+			  List<Account> accountList = new ArrayList<>();
 
-		        	
+	 
 
-				for(Map.Entry<Integer, Account> entry : accountMap.entrySet()) {
+			  for(Map.Entry<Integer, Account> e : accountMap.entrySet()) {
 
-					Integer Key = entry.getKey();
+	 
 
-		        	Account val = entry.getValue();
+				  if(e.getValue().getLocation().equals(location)) {
 
-		        	if(val.getSalary()>10000 && val.getSalary() < 20000) {
+	 
 
-		        		System.out.println("---------------------by salary----------");
+					  accountList.add(e.getValue());
 
-		        		System.out.println("");
+	 
 
-		        		System.out.println("AccountId"+"  "+"AccountHolderName"+"  "+"Salarybtw(100000,20000)");
+				  }
 
-		        		System.out.println(Key+"   "+val.getAccountHolderName()+"   "+val.getSalary());
+	 
 
-		        	}
+			  }
 
-				}
+	 
 
-				for(Map.Entry<Integer, Account> entry : accountMap.entrySet()) {
+			  newAccount.put(location, accountList);
+			  System.out.println(newAccount+"    ");
 
-					Integer Key = entry.getKey();
+	 
 
-		        	Account val = entry.getValue();
+	 
 
-		        	if(val.getAccountId()==3) {
-
-		        		System.out.println("----------------by account id--------");
-
-		        		System.out.println("");
-
-		        		System.out.println("AccountId"+"  "+"AccountHolderName"+"  "+"Salary");
-
-		        		System.out.println(Key+"   "+val.getAccountHolderName()+"   "+val.getSalary());
-
-		        	}
-
-		        	
-
-				}
-
-			}
-
+		}
 	}
+
+
+
+
+
